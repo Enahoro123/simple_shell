e073b0f15b22a4c2109b0937cd890930b300cc9b
Enahoro123 <uvieeu@gmailcom>
2022-08-11 16:32:16 -0700

    tasks corrected

--- shell.h
+++ shell.h
@@ -1,50 +1,105 @@
-#ifndef _SHELL_H_
-#define _SHELL_H_
-#include <dirent.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <signal.h>
+#ifndef SHELLH
+#define SHELLH
+
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/resource.h>
-#include <sys/stat.h>
-#include <sys/time.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/wait.h>
-#include <unistd.h>
-#include "linklist.h"
-#include "./memofile/memo.h"
-#include "shellvar.h"
-#include "./builtinfile/builtin.h"
-#include "./errorfile/error.h"
-#include "./shellhelperfile/shellhelper.h"
-#include "log.h"
-#define SPACE " "
-#define EQUAL "="
-#define COLON ":"
-#define BSLASH "\\"
-#define FSLASH "/"
-#define BUF_SIZE 4096
-#define PS1 write(STDOUT_FILENO, "MANDALORIAN$ ", 4)
-#define PS2 write(STDOUT_FILENO, "\nMANDALORIAN$ ", 5)
-#define NEWLINE write(STDOUT_FILENO, "\n", 1)
-#define DELIM " \t"
-/* _getline.c */
-ssize_t _getline(char **lineptr, size_t *n, int fd, list_t **mt);
-void reset_buffer(char *buffer, ssize_t buf_size);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-char *_strdup(char *str, list_t **mt);
-unsigned int _strspn(char *s, char *accept);
-char *_strpbrk(char *s, char *accept);
-char *_strtok(char *s, const char *delim);
-unsigned int _strlen(char *s);
-/* string02.c */
-int _strcmp(char *s1, char *s2);
-int _atoi(char *s);
-char *_strcat(char *s1, char *s2, list_t **mt);
-char *_itoa(ssize_t num, list_t **mt);
-char *reverse_str(char *s);
-/* string03.c */
+#include "history.h"
+#include "shellvars.h"
+/*#include <string.h>*/
+
+/* from in.c */
+int shintmode(void);
+
+/* from _printenv.c */
+int _printenv(void);
+
+/* from cmdcall.c */
+int builtincall(char *av[]);
+int cmdcall(char *av[], char *path);
+
+/* from parser.c */
+int parseargs(char **buf);
+
+/* from errhandl.c */
+int errhandl(int status);
+
+/* from string.c */
+size_t _strlen(char *str);
+char *_strcpy(char *dest, char *src);
+int _strcmp(char *, char *);
+char *_strdup(char *str);
+char *_strcat(char *a, char *b);
+
+/* from _getenv.c and getenviron.c */
+char ***getenviron(void);
+int setallenv(char **environ, char *add);
+char *_getenv(char *avzero);
+int _setenv(char *name, char *val);
+int _unsetenv(char *name);
+char **getallenv(void);
+
+
+/* from utility.c */
+char *itos(int digits);
 char *_strchr(char *s, char c);
-void _chartostr(char *s, char c);
-#endif /* _SHELL_H_ */
+int fprintstrs(int fd, char *str, ...);
+int printerr(char *);
+int linecount(int);
+
+/* from cd.c */
+int _cd(char *av[]);
+
+/* from alias.c */
+int aliascmd(char **av);
+char *getalias(char *name);
+int unsetalias(char *name);
+
+/* from shellvars.c */
+int initsvars(int ac, char **av);
+char *getsvar(char *name);
+int setsvar(char *name, char *val);
+int unsetsvar(char *name);
+ShellVar **getspecial(void);
+ShellVar **getvars(void);
+
+/* from _realloc.c */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
+/* from _strtok.c */
+char *strtok(char *str, char *delim);
+
+/* from _getline.c */
+int _getline(char **lineptr, int fd);
+
+char *strtokqe(char *str, char *delim, int escflags);
+
+/*from history.c*/
+int sethist(char *cmd);
+int print_hist(void);
+int exit_hist(void);
+
+
+/* from _printenv.c */
+int _printenv(void);
+int _putchar(char c);
+
+
+
+/*from help.c*/
+int help(char *cmd);
+
+/* from exitcleanup.c */
+void exitcleanup(char **av);
+
+/* from _atoi*/
+int _atoi(char *s);
+
+char *_getpid(void);
+
+
+#endif
